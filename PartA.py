0005ecdad108a802970f5b186e9c93783555dfda
ttristann <tristangalang12@gmail.com>
2024-10-02 13:31:07 -0700

    working tokenizer for a sample string

--- PartA.py
+++ PartA.py
@@ -1,6 +1,21 @@
+import re
+from collections import defaultdict
 """
 TO DO:
-- create a new repo in personal account
-- copy and paste from personal repo to openlab account
+- play around with regex code
 """
-print("hello world again")
+# def tokenize(textFilePath):
+#     with open(textFilePath, 'r') as main_file:
+#         main_text = main_file.read()
+    
+#     tokens_list = re.findall(r'\b[a-zA-Z0-9]+\b', main_text)
+#     print("inside\n")
+#     print(tokens_list)
+    
+#     return tokens_list
+
+# print(tokenize("/Users/tristangalang/Desktop/ICS/CS121/A1 - Text Processing/practice_file.txt"))
+
+practice_string = "This isn't a practice for 5 dim-wits at the University of California, Irvine."
+tokens = re.findall(r'\b[a-zA-Z0-9]+\b', practice_string)
+print(tokens)
