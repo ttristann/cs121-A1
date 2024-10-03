e02b70de0b74fb3139f43d342e18a7edd031787e
ttristann <tristangalang12@gmail.com>
2024-10-02 17:40:33 -0700

    finished the full functionality of tokenize function

--- PartA.py
+++ PartA.py
@@ -1,21 +1,17 @@
 import re
 from collections import defaultdict
 """
-TO DO:
-- play around with regex code
+Automatically opens, closes, and reads the text inside the input file.
+After reading it and converting the text to a string object, regex is
+used to filter to find the alphanumeric text to be converted into token. 
+
+Since this function goes through the whole input text file to filter only
+alphanumeric text, the runtime complexity of is O(n)
 """
-# def tokenize(textFilePath):
-#     with open(textFilePath, 'r') as main_file:
-#         main_text = main_file.read()
+def tokenize(TextFilePath):
+    with open(TextFilePath, 'r') as main_file:
+        main_text = main_file.read()
     
-#     tokens_list = re.findall(r'\b[a-zA-Z0-9]+\b', main_text)
-#     print("inside\n")
-#     print(tokens_list)
+    tokens_list = re.findall(r'\b[a-zA-Z0-9]+\b', main_text)
     
-#     return tokens_list
-
-# print(tokenize("/Users/tristangalang/Desktop/ICS/CS121/A1 - Text Processing/practice_file.txt"))
-
-practice_string = "This isn't a practice for 5 dim-wits at the University of California, Irvine."
-tokens = re.findall(r'\b[a-zA-Z0-9]+\b', practice_string)
-print(tokens)
+    return tokens_list
