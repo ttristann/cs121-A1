269a52295b9807f692a8860a0b81044e38a1c7f8
ttristann <tristangalang12@gmail.com>
2024-10-05 13:40:13 -0700

    corrected the comments and time complexity explanations

--- PartA.py
+++ PartA.py
@@ -1,40 +1,37 @@
-import re
+import os
 from collections import defaultdict
 """
 Automatically opens, closes, and reads the text inside the input file.
-After reading it and converting the text to a string object, regex is
-used to filter to find the alphanumeric text to be converted into token.
-Throws an error if the input is not a valid text file path.
+Each line is processed one at a time to efficiently use memory usage 
+and then each character is read from each line to determine if the 
+character is alphanumeric. If so, it becomes token and gets yielded,
+while other characters are ignored. 
 
-Since this function goes through the whole input text file to filter only
-alphanumeric text, the runtime complexity of the function is O(n).
+Since this function goes through the whole input text file, looking at 
+every character of each line to filter only alphanumeric text, the 
+runtime complexity of the function is O(n).
 """
 def tokenize(TextFilePath):
-    try:
-        with open(TextFilePath, 'r') as main_file:
-            current_token = ""
-            for line in main_file:
-                for char in line:
-                    if char.isalnum():
-                        current_token += char.lower()
-                    else:
-                        if current_token:
-                            yield current_token
-                            current_token = ""
+    with open(TextFilePath, 'r') as main_file:
+        current_token = ""
+        for line in main_file:
+            for char in line:
+                if char.isalnum():
+                    current_token += char.lower()
+                else:
+                    if current_token:
+                        yield current_token
+                        current_token = ""
 
-            if current_token:
-                yield current_token
-        
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Invalid Path: {TextFilePath}")
-        return
+        if current_token:
+            yield current_token
 
 """
 Creates a default dictionary object to keep track the frequencies of 
-the tokens while iterating through the list of tokens that are
-passed to the function. 
+the tokens while iterating through the stream of tokens that are 
+generated or yielded from the tokenize function. 
 
-The function iterates through the passed argument of list of tokens,
+The function iterates through stream of tokens of varying sizes,
 so the runtime complexity of this function is O(n). 
 """
 def computeWordFrequencies(tokens): 
@@ -65,7 +62,7 @@ def printFrequencies(token_dict):
 """
 Main program that executes all of the defined functions above.
 It takes an input from the terminal for a path or name of the
-text file to be processed, calls the defined functions above 
+text file to be processed and validated, calls the defined functions above 
 with the proper arguments stemming from the input, and prints
 the token frequencies. 
 """
@@ -75,12 +72,9 @@ if __name__ == '__main__':
     of the loop, otherwise it will loop back to ask for input again.
     """
     while True:
-        try: 
-            main_input = input("Path or Name of the text file: ")
-            tokens = tokenize(main_input)
-            break
-        except FileNotFoundError as error:
-            print(error)
-
+        main_input = input("Path or Name of the text file: ")
+        if os.path.exists(main_input) : break
+        else: print(f"Invalid File: {main_input}")
+    tokens = tokenize(main_input)
     token_dict = computeWordFrequencies(tokens)
     printFrequencies(token_dict)
